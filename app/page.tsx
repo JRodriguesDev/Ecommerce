a451542f052aa73e2bbf65d64705ba7f89ea0b62
José R. <0joserodrigues0@gmail.com>
2026-01-09 03:27:21 -0300

    feat: add basic home skeleton

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 export default function Home() {
   return (
-    <>
-      <h1>Hello Wolrd</h1>
-    </>
+    <main>
+      <header>
+        <h1>Ecommerce</h1>
+      </header>
+      <section>
+        <h2>Produtos</h2>
+        <article>Produto 1</article>
+        <article>Produto 2</article>
+        <article> Produto 3</article>
+      </section>
+    </main>
   );
 }
