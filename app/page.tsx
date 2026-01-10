e51e3c00ef63dfd627e2e9e09409e500fa27f185
José R. <0joserodrigues0@gmail.com>
2026-01-10 04:04:53 -0300

    feature: add header

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
+import Header from "@/components/layout/header";
+
 export default function Home() {
   return (
     <main>
-      <header>
-        <h1>Ecommerce</h1>
-      </header>
-      <section>
-        <h2>Produtos</h2>
-        <article>Produto 1</article>
-        <article>Produto 2</article>
-        <article> Produto 3</article>
-      </section>
+      <Header/>
     </main>
   );
 }
---
+++ components/layout/header.tsx
@@ -0,0 +1,29 @@
+import { FaUserCircle, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { TiStarFullOutline } from "react-icons/ti";
+import { BiCategory } from "react-icons/bi";
+
+
+export const Header = () => {
+    return (
+        <header className="bg-blue-900 w-full h-24 flex flex-col">
+            <div className="w-full flex flex-1 flex-row items-center justify-around gap-10">
+                <h1 className="cursor-pointer text-black text-2xl font-semibold hover:scale-105 duration-75">Ecommerce</h1>
+                <div className="relative w-2/6 flex items-center">
+                <BiCategory className="text-black text-3xl absolute -left-12 cursor-pointer hover:scale-110 duration-75"/>
+                    <input 
+                        className="bg-white w-full text-black rounded border-4 border-black hover:shadow-2xl hover:scale-105 duration-75" 
+                        type="text" 
+                        placeholder="Digite o Nome do Produto " />
+                    <FaSearch className="absolute right-2.5 text-black cursor-pointer"/>
+                </div>
+                <nav className="flex items-center gap-6">
+                    <button className="flex items-center gap-2 cursor-pointer hover:scale-110 duration-100 text-black font-semibold"><TiStarFullOutline className="text-yellow-300"/> Favoritos</button>
+                    <button className="flex items-center gap-2 cursor-pointer hover:scale-110 duration-100 text-black font-semibold"> <FaShoppingCart className="text-gray-300"/> Carrinho</button>
+                    <button className="text-black text-3xl hover:scale-105 duration-100 cursor-pointer"><FaUserCircle/></button>
+                </nav>
+            </div>    
+        </header>
+    )
+}
+
+export default Header
