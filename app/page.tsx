bb993be8e669f340ebc229036e851b960b4cde74
José R. <0joserodrigues0@gmail.com>
2026-01-12 00:37:05 -0300

    refactor: small reorganize

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,7 @@
-import Header from "@/components/layout/header";
-import Footer from "@/components/layout/footer";
-
 const Home = () => {
   return (
     <main>
-      <p>Rota /</p>
+      
     </main>
   );
 }
