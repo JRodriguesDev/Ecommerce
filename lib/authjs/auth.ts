3af76cdbeb9bfe1e1e857354f60786789eaaf0d6
jose <0joserodrigues0@gmail.com>
2026-01-19 02:41:58 -0300

    fix(auth): set session strategy to database

--- lib/authjs/auth.ts
+++ lib/authjs/auth.ts
@@ -1,8 +1,12 @@
 import nextAuth from 'next-auth'
 import Google from 'next-auth/providers/google'
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from '../prisma/index'
 import Credentials from './providers/credentials'
 
 export const {handlers, signIn, signOut, auth} = nextAuth({
+    adapter: PrismaAdapter(prisma),
+    session: {strategy: 'database'},
     providers: [
         Credentials,
         Google
